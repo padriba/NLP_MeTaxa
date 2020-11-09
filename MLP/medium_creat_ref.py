a399117bbd18426057d639f2b705d6d72e7e9688
padriba <brahim.matougui@gmail.com>
2020-11-09 12:04:06 +0100

    Update medium_creat_ref.py

--- MLP/medium_creat_ref.py
+++ MLP/medium_creat_ref.py
@@ -4,13 +4,10 @@ import csv
 ncbi = NCBITaxa()
 
 
-#dataset = pd.read_csv('/bettik/matouguib/cami/fasta/results/CSV_files/metagenomics_signatures_1-8_CAMI_low_reduced_mean/metagenomics_signatures_1-8_CAMI_low_reduced_mean.csv')
-#dataset = pd.read_csv('/bettik/matouguib/cami/fasta/results/CSV_files/metagenomics_signatures_1-8_CAMI_MED_reduced_mean/metagenomics_signatures_1-8_CAMI_MED_reduced_mean.csv')
-#dataset = pd.read_csv('/bettik/matouguib/cami/fasta/results/CSV_files/metagenomics_signatures_1-8_CAMI_HIGH_reduced_mean/metagenomics_signatures_1-8_CAMI_HIGH_reduced_mean.csv')
 
-#dataset = pd.read_csv('/bettik/matouguib/cami/fasta/results/metagenomics_signatures_1-8_CAMI_HIGH_mean/metagenomics_signatures_1-8_CAMI_HIGH_mean.csv')
-dataset = pd.read_csv('metagenomics_signatures_1-8_CAMI_MED_mean_1123232.csv')
-#dataset = pd.read_csv('/bettik/matouguib/cami/fasta/results/metagenomics_signatures_1-8_CAMI_low_mean/metagenomics_signatures_1-8_CAMI_low_mean.csv')
+
+dataset = pd.read_csv('MLP/metagenomics_signatures_1-8_CAMI_MED_mean_1123232.csv')
+
 
 
 #dataset = pd.read_csv('/bettik/matouguib/cami/fasta/results/metagenomics_signatures_1-8_CAMI_low_mean/metagenomics_signatures_1-8_CAMI_low_mean.csv', header=None)
@@ -22,7 +19,7 @@ y = dataset.iloc[:, 0].values
 #     mydict = {rows[0]:rows[1] for rows in reader}
 
 print(len(y))
-fileoutput = open('reference_medium.txt','w+')
+fileoutput = open('MLP/reference_medium.txt','w+')
 for i in range(len(y)):
     lineage = ncbi.get_lineage(y[i])
     result=[ncbi.get_rank([taxid]) for taxid in lineage]
